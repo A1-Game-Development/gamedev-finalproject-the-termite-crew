e996ddcd32f24a4b2e322c6492dccc1947c1a2c5
Raw Fish <zach.otto.schmalz@gmail.com>
2026-04-28 09:05:29 -0600

    Unfinished Camera Movement
    reading api docs help you write better code!?

--- TermiteSimulator/Assets/CameraControl.cs
+++ TermiteSimulator/Assets/CameraControl.cs
@@ -7,14 +7,25 @@ public class CameraControl : MonoBehaviour
     public float maxX;
     public float minY;
     public float maxY;
+    
+    [Header("Input")]
+    public float horizontalInput = Input.GetAxis("Horizontal");
+    public float verticalInput = Input.GetAxis("Vertical");
 
     [Header("Camera")]
-    public float speed; //start with 20
+    public float movementSpeed;
     private Vector2 motion;
+    public GameObject toTrack;
 
-    void Update()
+    void LateUpdate()
     {
-        motion = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        transform.Translate(motion * speed * Time.deltaTime);
+        // motion = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        // transform.Translate(motion * movementSpeed * Time.deltaTime);
+        // transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, verticalInput * movementSpeed * Time.deltaTime, 0);
+
+        transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, verticalInput * movementSpeed * Time.deltaTime, 0);
+        // Mathf.Clamp(transform.position.x, maxX, minX);
+        // Mathf.Clamp(transform.position.y, maxY, minY);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, maxX, minX), Mathf.Clamp(transform.position.y, maxY, minY), 0);
     }
 }
